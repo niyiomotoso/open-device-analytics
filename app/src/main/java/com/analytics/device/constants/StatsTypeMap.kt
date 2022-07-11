d2c681bc7731dbe94e4790a75fd50662bfc36f9d
Niyi Omotoso <omoniyiomotoso@gmail.com>
2022-07-11 10:19:00 +0200

    statistics map for randomization types

---
+++ app/src/main/java/com/analytics/device/constants/StatsTypeMap.kt
@@ -0,0 +1,7 @@
+package com.analytics.device.constants
+
+class StatsTypeMap {
+    val  USAGE = "App Usage Count"
+    val LAST_TIME_OPENED = "Last Time Opened"
+    val TOTAL_TIME_USED = "Total Time Used"
+}
